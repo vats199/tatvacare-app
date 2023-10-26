7c81b181bd646fb4e698e2fed7c4ab51e4661282
TheCodingStudio <thecodingstudio@TheCodingStudios-Mac-mini.local>
2023-10-26 18:14:58 +0530

    insights screen ui completed with bottom btn

--- src/screens/Spirometer/ExerciseInsights.tsx
+++ src/screens/Spirometer/ExerciseInsights.tsx
@@ -2,11 +2,54 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import InsightsScreen from '../../components/molecules/InsightsScreen';
 import {HealthInsightsList} from './HealthInsightsScreen';
+import {Fonts, Matrics} from '../../constants';
+import {colors} from '../../constants/colors';
+import Button from '../../components/atoms/Button';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const ExerciseInsights = () => {
-  return <InsightsScreen data={HealthInsightsList} />;
+  const insets = useSafeAreaInsets();
+  return (
+    <>
+      <InsightsScreen data={HealthInsightsList} />
+      <View
+        style={[
+          styles.bottomBtnContainerShadow,
+          styles.bottonBtnContainer,
+          {
+            paddingBottom: insets.bottom !== 0 ? insets.bottom : Matrics.vs(16),
+          },
+        ]}>
+        <Button
+          title="Connect"
+          titleStyle={styles.saveBtnTxt}
+          buttonStyle={{
+            borderRadius: Matrics.s(19),
+          }}
+          onPress={() => {}}
+        />
+      </View>
+    </>
+  );
 };
 
 export default ExerciseInsights;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  bottomBtnContainerShadow: {
+    shadowColor: colors.black,
+    shadowOffset: {width: 0, height: 0},
+    shadowOpacity: 0.08,
+    shadowRadius: 8,
+    elevation: 2,
+  },
+  bottonBtnContainer: {
+    backgroundColor: colors.white,
+    paddingVertical: Matrics.vs(8),
+  },
+  saveBtnTxt: {
+    fontFamily: Fonts.BOLD,
+    fontSize: Matrics.mvs(16),
+    fontWeight: '700',
+  },
+});
